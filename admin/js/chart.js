6d4a8bfcfd776799d92ea89c36777bd029e2a6c6
dayoung <leedayeonggg@gmail.com>
2024-04-26 18:32:43 +0900

    [mod]우예지/대쉬보드 - 그리드

--- admin/js/chart.js
+++ admin/js/chart.js
@@ -1,148 +1,148 @@
-/*
-  =====================대쉬보드 
-*/
-const lineChart = $('#line-chart');
-const barChart = $('#bar-chart');
-const pieChart = $('#pie-chart');
-const barChart2 = $('#bar-chart2');
+document.addEventListener("DOMContentLoaded", function() {
 
-// 이달의 매출
-new Chart(barChart, {
-  type: 'bar',
-  data: {
-    labels: ['html', 'css', 'javascript', 'php', 'rect'],
-    datasets: [
-      {
-        label: '5월',
-        data: [4, 12, 8, 7, 2, 5],
-        borderWidth: 1,
-      }
-    ]
-  },
-  options: {
-    cutout:'90%',
-    maintainAspectRatio:false,
-    plugins: {
-      legend: {
-          Position: 'bottom',
-          display: true,
-          labels: {
-              color: '#472ea1',
-              font: {
-                size: 16
-            }
-          }
-      }
-    }
+  /*
+    =====================대쉬보드 
+  */
+  const lineChart = $('#line-chart');
+  const barChart = $('#bar-chart');
+  const pieChart = $('#pie-chart');
+  const barChart2 = $('#bar-chart2');
+
+  // 이달의 매출
+  const sales2022 = {
+    label: '2022년',
+    data: [90, 86, 89, 93, 95,95,95,95,95,95,95,95,],
+    borderWidth: 2
   }
-});
-
-// 회원관리
-new Chart(lineChart, {
-  type: 'line',
-  data: {
-    labels: ['1월', '2월', '3월', '4월', '5월', '6월'],
-    datasets: [
-        {
-        label: '2023',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-        },
-        {
-          label: '2024',
-          data: [4, 12, null, 7, 10, 5],
-          borderWidth: 2,
-          // hoverBorderWidth:5,
-          // borderColor: 'rgba(0,0,0,0.5)',
-          // backgroundColor:'yellow',
-          // radius:4,
-          // hoverRadius:10,
-          // pointBorderColor:'black',
-          // pointStyle:sun,
-          // showLine:true,
-          spanGaps:true,
-          // stepped:true
-        }
-    ]
-  },
-  options: {
-    scales: {
-      y: {
-        // beginAtZero: true
-        stacked:true
-      }
+  const sales2023 = {
+    label: '2023년',
+    data: [70, 76, 79, 73, 35,35,35,35,35,35,35,35,],
+    borderWidth: 2
+  }
+  const sales2024 = {
+    label: '2024년',
+    data: [60, 66, 69, 63, 65,],
+    borderWidth: 2
+  }
+  new Chart(barChart, {
+    type: 'line',
+    data: {
+      labels: ['1월', '2월', '3월', '4월', '5월','6월','7월','8월','9월','10월','11월','12월'],
+      datasets: [
+        sales2022,sales2023,sales2024
+      ]
     },
-    maintainAspectRatio:false
-  }
-});
-const data2023 = {
-  label: '2023',
-  data: [12, 19, 24, 7, 10, 5],
-  borderWidth: 2
-}
-const data2024 = {
-  label: '2024',
-  data: [4, 12, 8, 7, 10, 5],
-  borderWidth: 1,
-  backgroundColor: [
-    "rgba(242,166,54,.5)",
-    "rgba(39,79,76,.5)",
-    "rgba(40,161,130,.5)",
-    "rgba(206,29,22,.5)",
-    "rgba(242,166,54,.5)",
-    "rgba(39,79,76,.5)"
-  ],                
-  borderColor: [
-      "rgba(242,166,54,1)",
-      "rgba(39,79,76,1)",
-      "rgba(40,161,130,1)",
-      "rgba(206,29,22,1)",
-      "rgba(242,166,54,1)",
-      "rgba(39,79,76,1)"
-  ]
-}
-
-//강의관리
-new Chart(barChart2, {
-  type: 'bar',
-  data: {
-    labels: ['1월', '2월', '3월', '4월', '5월'],
-    datasets: [
-      {
-        label: '2024',
-        data: [4, 12, 8, 7, 10, 5],
-        borderWidth: 1,
-      }
-    ]
-  },
-  options: {
-    cutout:'90%',
-    maintainAspectRatio:false,
-    plugins: {
-      legend: {
-          Position: 'bottom',
-          display: true,
-          labels: {
-              color: '#fff',
-              font: {
-                size: 20
+    options: {
+      cutout:'90%',
+      maintainAspectRatio:false,
+      plugins: {
+        legend: {
+            Position: 'bottom',
+            display: true,
+            labels: {
+                color: '#222',
+                font: {
+                  size: 16
+              }
             }
-          }
+        }
       }
     }
-  }
-});
+  });
 
-//수료관리
-new Chart(pieChart, {
-  type: 'pie',
-  data: {
-    labels: ['1월', '2월', '3월', '4월', '5월'],
-    datasets: [
-      data2024
-    ]
-  },
-  options: {
-    maintainAspectRatio:false
+  // 회원관리
+  const member1 = {
+    label: '총회원수',
+    data: [90, 86, 89, 93, 95, 85],
+    borderWidth: 2
   }
+  const member2 = {
+    label: '신규 회원',
+    data: [30, 19, 24, 50, 55, 25],
+    borderWidth: 2
+  }
+  const member3 = {
+    label: '탈퇴회원',
+    data: [1, 1, 3, 3, 3, 4],
+    borderWidth: 2
+  }
+
+  new Chart(lineChart, {
+    type: 'line',
+    data: {
+      labels: ['1월', '2월', '3월', '4월', '5월', '6월'],
+      datasets: [member1,member2,member3]
+    },
+    options: {
+      scales: {
+        y: {
+          // beginAtZero: true
+          stacked:false
+        }
+      },
+      maintainAspectRatio:false
+    }
+  });
+
+
+  //강의관리
+
+  const age20 = {
+    label: '20대',
+    data: [40, 50, 70, 20, 60, 65],
+    borderWidth: 2
+  }
+  const age30 = {
+    label: '30대',
+    data: [10, 10, 80, 10, 90, 95],
+    borderWidth: 2
+  }
+  const age40 = {
+    label: '40-60대',
+    data: [20, 20, 10, 30, 20, 15],
+    borderWidth: 2
+  }
+  new Chart(barChart2, {
+    type: 'bar',
+    data: {
+      labels: ['html', 'css', 'javascript', 'php', 'rect','python'],
+      datasets: [age20,age30,age40]
+    },
+    options: {
+      cutout:'90%',
+      maintainAspectRatio:false,
+      plugins: {
+        legend: {
+            Position: 'bottom',
+            display: true,
+            labels: {
+                color: '#222',
+                font: {
+                  size: 16
+              }
+            }
+        }
+      }
+    }
+  });
+
+  //수료관리
+  const Completion = {
+    label: '2024',
+    data: [75, 80, 15, 5],
+    borderWidth: 2,
+  }
+  new Chart(pieChart, {
+    type: 'pie',
+    data: {
+      labels: ['수료완료', '진행중', '미수료', '중도취소'],
+      datasets: [
+        Completion
+      ]
+    },
+    options: {
+      maintainAspectRatio:false
+    }
+  });
+  
 });
