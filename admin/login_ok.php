e0e7a9565259f1cbbea112e17363755912bc1a48
WooYeji <131832439+dubumom@users.noreply.github.com>
2024-04-28 11:45:38 +0900

    닷홈에 올려줘

--- admin/login_ok.php
+++ admin/login_ok.php
@@ -1,6 +1,6 @@
 <?php
   session_start();
-  include_once $_SERVER['DOCUMENT_ROOT'] . '/ccccoding/inc/dbcon.php';
+  include_once $_SERVER['DOCUMENT_ROOT'] . '/ccccoding/admin/inc/dbcon.php';
 
 
   $userid = trim($_POST['userid']);
