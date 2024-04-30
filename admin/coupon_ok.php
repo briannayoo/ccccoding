f2ff35071c8b49694e44c7e5ba3fc8715b503299
WooYeji <131832439+dubumom@users.noreply.github.com>
2024-05-01 01:49:58 +0900

    쿠폰이미지 테스트

--- admin/coupon_ok.php
+++ admin/coupon_ok.php
@@ -2,8 +2,10 @@
 session_start();
 require_once $_SERVER['DOCUMENT_ROOT'] . '/ccccoding/admin/inc/dbcon.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/ccccoding/admin/inc/admin_check.php';
-ini_set( 'display_errors', '0' );
+ini_set( 'display_errors', '1' );
 
+$mysqli->autocommit(FALSE);//커밋이 안되도록 지정
+try {
 $coupon_name = $_POST['coupon_name'];
 $coupon_desc = $_POST['coupon_desc']; // $coupon_name 변수를 $coupon_desc로 수정
 $coupon_type = $_POST['coupon_type'];
@@ -18,37 +20,39 @@ $use_date_type = $_POST['use_date_type'] ?? '';
 $start_date = $_POST['start_date'];
 $end_date = $_POST['end_date'];
 
-//$coupon_image = ""; // 쿠폰 이미지 변수 초기화
-
-if($_FILES["coupon_image"]["name"]) { // 첨부한 파일이 있으면
+$coupon_image = ""; // 쿠폰 이미지 변수 초기화
 
   if($_FILES['coupon_image']['size']>10240000) { // 10메가
       echo "<script>alert('10메가 이하만 첨부할 수 있습니다.');history.back();</script>";
       exit;
   }
 
-  if($_FILES['coupon_image']['type']!='image/jpeg' and $_FILES['coupon_image']['type']!='image/gif' and $_FILES['coupon_image']['type']!='image/png') { // 이미지가 아니면, 다른 type은 and로 추가
-      echo "<script>alert('이미지만 첨부할 수 있습니다.');history.back();</script>";
+  // if($_FILES['coupon_image']['type']!='image/jpeg' and $_FILES['coupon_image']['type']!='image/gif' and $_FILES['coupon_image']['type']!='image/png') { // 이미지가 아니면, 다른 type은 and로 추가
+  //     echo "<script>alert('이미지만 첨부할 수 있습니다.');history.back();</script>";
+  //     exit;
+  // }
+
+    //이미지 여부 검사
+    if (strpos($_FILES['coupon_image']['type'], 'image') === false) {
+      echo "<script>
+        alert('이미지만 업로드해주세요');
+        history.back();
+      </script>";
       exit;
-  }
+    }
 
   $save_dir = $_SERVER['DOCUMENT_ROOT']."/ccccoding/admin/c_upload/"; // 파일을 업로드할 디렉토리
   $filename = $_FILES["coupon_image"]["name"];
   $ext = pathinfo($filename,PATHINFO_EXTENSION); // 확장자 구하기
-  $newfilename = "CPN_".date("YmdHis").substr(rand(),0,6);
-  $coupon_image = $newfilename.".".$ext; // 새로운 파일이름과 확장자를 합친다
+  $newfilename = date("YmdHis").substr(rand(),0,6);
+  $savefile = $newfilename.".".$ext; // 새로운 파일이름과 확장자를 합친다
  
-  if(move_uploaded_file($_FILES["coupon_image"]["tmp_name"], $save_dir.$coupon_image)) {
-      $coupon_image = "/ccccoding/admin/c_upload/".$coupon_image;
+  if(move_uploaded_file($_FILES["coupon_image"]["tmp_name"], $save_dir.$savefile)) {
+      $coupon_image = "/ccccoding/admin/c_upload/".$savefile;
   } else {
-      echo "<script>alert('이미지를 등록할 수 없습니다. 관리자에게 문의해주십시오.');history.back();</script>";
+      echo "<script>alert('이미지를 등록할 수 없습니다. 관리자에게 문의해주십시오.');//history.back();</script>";
       exit;
   }
-}
-
-$mysqli->autocommit(FALSE); // 커밋이 안되도록 지정
-
-try {
 
   $sql = "INSERT INTO coupons (
     coupon_name, 
@@ -82,14 +86,12 @@ try {
     '{$end_date}'
   )";
 
-echo $sql;
-  // $rs = $mysqli->query($sql) or die($mysqli->error);
-  // $pid = $mysqli->insert_id;
+//echo $sql;
+  $rs = $mysqli->query($sql);
+  $mysqli->commit(); // 디비에 커밋한다.
 
-  // $mysqli->commit(); // 디비에 커밋한다.
-
-  // echo "<script>alert('등록했습니다.');location.href='/ccccoding/admin/coupon.php';</script>";
-  // exit;
+  echo "<script>alert('등록했습니다.');location.href='/ccccoding/admin/coupon.php';</script>";
+  exit;
 
 } catch (Exception $e) {
   $mysqli->rollback(); // 저장한 테이블이 있다면 롤백한다.
