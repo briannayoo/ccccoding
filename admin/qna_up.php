1c68bbb523087259eebcffaab508dbcbbb693640
BRI <159090432+briannayoo@users.noreply.github.com>
2024-05-02 06:49:05 +0900

    유부현/ qna 댓글 수정

--- admin/qna_up.php
+++ admin/qna_up.php
@@ -4,28 +4,22 @@ session_start();
   include_once $_SERVER['DOCUMENT_ROOT'] . '/ccccoding/admin/inc/dbcon.php';
 
     //idx 번호의 글조회
-   $qno = $_GET['qid'];
-
+    $qno = $_GET['qid'];
     $qnaSql = "SELECT * FROM qna WHERE qid = {$qno}";
     $qnaResult = $mysqli -> query($qnaSql);
     $qa = $qnaResult -> fetch_object();
 
-
-
    //조회수 업데이트
- 
     $qnahit = $qa -> hit + 1;
     $qnaUpdate = "UPDATE qna SET hit={$qnahit} WHERE qid = {$qno}";
     $mysqli->query($qnaUpdate);
 ?>
-      <!-- 상단 until-list (e) -->
       <!-- sub-page-tit-area (s) -->
       <div class="page-tit-area">
         <h2 class="tit-h2">Q&amp;A 답변등록</h2>
       </div>
       <!-- sub-page-tit-area (e) -->
-
-      <div class="content"> <!--temp-area 빼야됨-->
+      <div class="content">
         <!-- 작성자 질문 -->
         <div class="d-flex">
           <div class="pro-box">
@@ -52,14 +46,13 @@ session_start();
             <p><?=$qa -> content; ?></p>
           </div>
         </div>
-
         <!-- 답변등록 -->
-        <?php
-        $reply_sql = "SELECT * FROM qna WHERE r_idx = {$qno} order by idx desc";
+      <?php
+        $rno = $_GET['idx'];
+        $reply_sql = "SELECT * FROM qna_reply WHERE r_idx = {$rno} order by idx desc";
         $reply_result = $mysqli->query($reply_sql);
         while($reply_row = mysqli_fetch_assoc($reply_result)){
-        ?>
-
+      ?>
 <dialog class="edit_dialog">
   <!-- 댓글 수정 폼 -->
   <form action="qna_replymod_ok.php" method="POST">
@@ -81,7 +74,7 @@ session_start();
 </dialog>
 </div><!--// reply -->
 
-<form action="reply_ok.php" method="POST">
+<form action="qna_reply_ok.php" method="POST">
   <div class="answer d-flex">
     <div class="a-box box-shadow box">
       <textarea class="a-boxform form-control" id="txtarea" placeholder="내용을 입력하세요.">
