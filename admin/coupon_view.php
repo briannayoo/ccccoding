accf2f3fccf882d967278ea7a4c7f6d4b0455ad3
psh38 <sosohy38@gmail.com>
2024-05-03 02:05:45 +0900

    [mod]박소현/쿠폰상세 수정

--- admin/coupon_view.php
+++ admin/coupon_view.php
@@ -35,7 +35,7 @@
           <div class="row">
             <label for="coupon_desc" class="col-md-1 col-form-label tit-h4">쿠폰설명</label>
             <div class="col-md-11">
-              <?=$rs -> coupon_name?>
+              <?=$rs -> coupon_desc?>
             </div>
           </div>
           <!-- input text 1 (e) -->
@@ -44,7 +44,7 @@
           <div class="row">
             <label for="use_min_price" class="col-md-1 col-form-label tit-h4">최소사용금액</label>
             <div class="col-md-11">
-              <?=$rs -> coupon_name?>
+              <?=$rs -> use_min_price?>
             </div>
           </div>
           <!-- input text 단위 1/3 (e) -->
@@ -53,7 +53,7 @@
           <div class="row">
             <label for="max_value" class="col-md-1 col-form-label tit-h4">최대할인금액</label>
             <div class="col-md-11">
-              <?=$rs -> coupon_name?>
+              <?=$rs -> max_value?>
             </div>
           </div>
           <!-- input text 단위 1/3 (e) -->
@@ -63,7 +63,13 @@
           <div class="row">
             <label for="coupon_type" class="col-md-1 col-form-label tit-h4">할인종류</label>
             <div class="col-md-11">
-              <?=$rs -> coupon_name?>
+                <?php
+                if ($rs->coupon_type == '1') {
+                    echo $rs->coupon_price . '원 할인';
+                } elseif ($rs->coupon_type == '2') {
+                    echo $rs->coupon_ratio . '% 할인';
+                }
+                ?>
             </div>
           </div>
           <!-- select + input text 혼합(e) -->
@@ -72,7 +78,7 @@
           <div class="row">
             <label for="use_date_type" class="col-md-1 col-form-label tit-h4">사용기한</label>
             <div class="col-md-11">
-              <?=$rs -> coupon_name?>
+              <?=$rs -> start_date?> ~  <?=$rs -> end_date?>
             </div>
           </div>
           <!-- 기간설정 시 (e) -->
@@ -80,7 +86,10 @@
           <div class="row">
             <label for="status" class="col-md-1 col-form-label tit-h4">상태</label>
             <div class="col-md-11">
-              <?=$rs -> coupon_name?>
+              <?php 
+                if($rs->status == 1){echo '활성화';}
+                if($rs->status == 2){echo '비활성화';}
+              ?>
             </div>
           </div>
 
@@ -98,7 +107,7 @@
 
           <!-- 하단버튼 (s) -->
           <div class="btn-area">
-            <a href="coupon_list.php" class="btn btn-primary btn-lg">확인</ㅁ>
+            <a href="coupon_list.php" class="btn btn-primary btn-lg">확인</a>
           </div>
           <!-- 하단버튼(e) -->
         </form>
