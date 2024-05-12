2470ef9e2d023947a627bf04a17706d1a75d18b8
BRI <159090432+briannayoo@users.noreply.github.com>
2024-05-12 17:30:31 +0900

    유부현 new

--- js/common.js
+++ js/common.js
@@ -57,6 +57,14 @@ $(function(){
     autoSlide()
   }
 
+  //Summer note
+    $('#summernote').summernote({
+    placeholder: '학습관련 질문을 입력해주세요. 서로 예의를 지키며 존중하는 문화를 만들어가요',
+    tabsize: 2,
+    height: 100
+    });
+
+
 
 
 
