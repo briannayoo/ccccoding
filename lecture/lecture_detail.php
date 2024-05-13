f4774c435abb8a37b380622f7e00353397b64334
WooYeji <131832439+dubumom@users.noreply.github.com>
2024-05-13 17:46:16 +0900

    [new]우예지/order,lecture php 생성

---
+++ lecture/lecture_detail.php
@@ -0,0 +1,110 @@
+<?php
+  $title = 'ccccoding';
+  include_once $_SERVER['DOCUMENT_ROOT'] . '/ccccoding/inc/header.php';
+?>
+  <!-- 우예지 (s) -->
+  <main>
+      <div class="lecture-detail">
+        <div class="container d-flex lecture-top">
+          <div>
+            <img src="image/img_lecture.png" alt="">
+            <a href="# " class="btn btn-primary btn-md">1분 미리보기</a>
+          </div>
+          <div>
+            <div class="d-flex gap-2 page-nav">
+              <span class="flag-state-incomplete">new</span>
+              <p class="txt-sm text-c6"><span>개발,프로그래밍</span><i class="fa-solid fa-angle-right fa-small"></i><span>프로그래밍 언어</span></p>
+            </div>
+            <h2 class="tit-h2">고수가 되는 파이썬 : 동시성과 병렬성 문법 배우기 Feat. 멀티스레딩 vs 멀티프로세싱 (Inflearn Original)</h2>
+            <p><i class="fa-solid fa-eye fa-small"></i>2.5만명의 수강생이 수강하고 있어요</p>
+            <p><i class="fa-solid fa-heart fa-small"></i>1.8만명의 수강생이 좋아해요</p>
+          </div>
+
+        </div>
+      </div>
+      <div class="container lecture-bottom">
+        <div class="d-flex justify-content-between">
+          <div>
+            <div class="lecture-detail-tit">
+              <h2 class="tit-h2">중급자를 위해 준비한 [프로그래밍 언어] 강의입니다.</h2>
+              <p class="tit-h4">기술면접 대비를 위해 OS 지식을 기반으로 멀티 스레딩 및 멀티프로세싱, 병렬성, 병행성 등의 문법을 다루기 위한 과정입니다. 다수의 자원으로 실행 효율을 높이는 방법에 대한 기반 지식을 학습합니다.</p>
+              <hr>
+            </div>
+              <h2 class="tit-h2">이런걸 배워요</h2>
+              <div class="d-flex align-items-center explan-box">
+                <div class="big-ico text-center"><i class="fa-solid fa-marker fa-big"></i><span class="tit-h5">무엇을<br>배우나요?</span></div>
+                <ul class="text-c4">
+                  <li><i class="fa-solid fa-check fa-small"></i>수준있는 운영체제 기반 성능 프로그래밍</li>
+                  <li><i class="fa-solid fa-check fa-small"></i>파이썬 실전 문법</li>
+                  <li><i class="fa-solid fa-check fa-small"></i>운영체제 OS 지식</li>
+                  <li><i class="fa-solid fa-check fa-small"></i>파이썬 기술 면접 대비 수준있는 지식</li>
+                  <li><i class="fa-solid fa-check fa-small"></i>개발자(엔지니어)를 위한 프로그래밍 지식</li>
+                  <li><i class="fa-solid fa-check fa-small"></i>기타 개발 관련 지식</li>
+                </ul>
+              </div>
+            <h2 class="tit-h2">이런분들께 추천드려요</h2>
+            <div class="d-flex align-items-center explan-box">
+              <div class="big-ico text-center"><i class="fa-solid fa-chalkboard-user fa-big"></i><span class="tit-h5">학습 대상은<br>누구일까요?</span></div>
+              <ul class="text-c4">
+                <li><i class="fa-solid fa-check fa-small"></i>파이썬 스레딩 및 멀티프로세싱을 배우고 싶은 분/li>
+                <li><i class="fa-solid fa-check fa-small"></i>파이썬을 보다 깊게 학습하고 싶은 모든 분</li>
+              </ul>
+            </div>
+            <div class="d-flex align-items-center explan-box">
+              <div class="big-ico text-center"><i class="fa-solid fa-book-open fa-big"></i><span class="tit-h5">선수 지식<br>필요한가요?</span></div>
+              <ul class="text-c4">
+                <li><i class="fa-solid fa-check fa-small"></i>파이썬 기초 과정을 학습 하신 분</li>
+                <li><i class="fa-solid fa-check fa-small"></i>ㅋㅋㅋ코딩 파이썬 입문 과정을 학습 하신 분</li>
+              </ul>
+            </div>
+          </div>
+            <!-- 결제 영역-->
+            <div class="order-payment">
+              <div class="order-pay cart-box">
+                <h3 class="text-center">이달의 할인! 놓칠 수 없는 기회에요</h3>
+                <hr>
+                <p class="text-red">15%<span class="text-c5">300,000</span></p>
+                <p class="tit-h3">255,000원</p>
+                <p class="text-c5"><i class="fa-solid fa-check fa-small"></i>2개월 무이자 할부 가능</p>
+                <button type="button" class="btn btn-outline-secondary btn-md pm-line mb-3">수강신청 하기</button>
+                <button type="button" class="btn etc-c btn-md mb-3">장바구니 담기</button>
+                <ul class="list-group etc-list justify-content-end">
+                  <li class="list-group-item"><a href="#"><i class="fa-solid fa-heart fa-small"></i>좋아요</a></li>
+                  <li class="list-group-item"><a href="#"><i class="fa-solid fa-shuffle fa-small"></i>공유하기</a></li>
+                </ul>
+              </div>
+              <div class="order-infor">
+                <p class="d-flex justify-content-between align-items-center text-c6"><span><i class="fa-solid fa-circle-question fa-small"></i><strong>구매자 정보</strong></span><span><a href="#" title="문의하기 바로가기">문의하기</a><i class="fa-solid fa-angle-right fa-small"></i></span></p>
+              </div>
+            </div>
+        </div>
+        
+          <h2 class="tit-h2">비슷한 강의를 추천드려요</h2>
+          <ul class="lecture_most">
+            <li><a href="#">
+              <img src="./image/img_lecture.png" alt="">
+              <h3 class="tit-h4">따라하며 배우는 리액트 A-Z</h3>
+              <p>이 강의를 통해 리액트 기초부터 중급까지 배우게 됩니다. 하나의 강의로 개념도 익히고 실습도 하며, 리액트를 위해 필요한 대부분의 지식을 한번에 습득할 수 있도록 만들었습니다.</p>
+            </a></li>
+            <li><a href="#">
+              <img src="./image/img_lecture.png" alt="">
+              <h3 class="tit-h4">따라하며 배우는 리액트 A-Z</h3>
+              <p>이 강의를 통해 리액트 기초부터 중급까지 배우게 됩니다. 하나의 강의로 개념도 익히고 실습도 하며, 리액트를 위해 필요한 대부분의 지식을 한번에 습득할 수 있도록 만들었습니다.</p>
+            </a></li>
+            <li><a href="#">
+              <img src="./image/img_lecture.png" alt="">
+              <h3 class="tit-h4">따라하며 배우는 리액트 A-Z</h3>
+              <p>이 강의를 통해 리액트 기초부터 중급까지 배우게 됩니다. 하나의 강의로 개념도 익히고 실습도 하며, 리액트를 위해 필요한 대부분의 지식을 한번에 습득할 수 있도록 만들었습니다.</p>
+            </a></li>
+            <li><a href="#">
+              <img src="./image/img_lecture.png" alt="">
+              <h3 class="tit-h4">따라하며 배우는 리액트 A-Z</h3>
+              <p>이 강의를 통해 리액트 기초부터 중급까지 배우게 됩니다. 하나의 강의로 개념도 익히고 실습도 하며, 리액트를 위해 필요한 대부분의 지식을 한번에 습득할 수 있도록 만들었습니다.</p>
+            </a></li>
+          </ul>
+        
+      </div>
+  </main>
+<?php
+  include_once $_SERVER['DOCUMENT_ROOT'] . '/ccccoding/inc/footer.php';
+?>
