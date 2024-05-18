8998d764728d1d14321dca3578d39affc9d8c02c
psh38 <sosohy38@gmail.com>
2024-05-19 04:18:20 +0900

    [mod]박소현/페이지네이션작업

--- mypage/payment.php
+++ mypage/payment.php
@@ -6,6 +6,28 @@
   $sql = "SELECT * FROM payments";
   $result = $mysqli->query($sql);
 
+  //총개수 조회
+  $sql = "SELECT COUNT(*) AS cnt FROM payments WHERE 1=1";
+  $result = $mysqli->query($sql);
+  $count = $result->fetch_object();
+
+  $totalcount = $count->cnt; //총검색건수
+  $targetTable = 'payments';
+  include_once $_SERVER['DOCUMENT_ROOT'] . '/ccccoding/inc/pagination.php';
+
+  //페이지네이션
+  $sql = "SELECT * FROM payments WHERE 1=1";
+  $order = " order by oid desc";
+  $sql .= $order;
+  $limit = " LIMIT  $startLimit, $endLimit";
+  $sql .= $limit;
+  // echo $sql;
+
+  $result = $mysqli->query($sql);
+  while ($rs = $result->fetch_object()) {
+    $rsArr[] = $rs;
+  }
+
   while ($rs = $result->fetch_object()) {
     $rsArr[] = $rs;
   }
@@ -113,6 +135,56 @@
         </div>
         <!-- nodata(e) -->
         <?php } ?>
+
+        <!-- pagination(s) -->
+        <nav aria-label="페이지네이션">
+          <ul class="pagination">
+            <li class="page-item <?php echo $pageNumber == 1 ? 'disabled' : ''; ?>">
+              <a class="page-link" href="<?php echo $pageNumber == 1 ? '#' : 'payment.php?pageNumber=1'; ?>" tabindex="-1">
+                <span class="visually-hidden">처음</span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-left" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
+                  <path fill-rule="evenodd" d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
+                </svg>
+              </a>
+            </li>
+            <li class="page-item <?php echo $pageNumber == 1 ? 'disabled' : ''; ?>">
+              <a class="page-link" href="<?php echo $pageNumber == 1 ? '#' : 'payment.php?pageNumber=' . ($pageNumber - 1); ?>" tabindex="-1">
+                <span class="visually-hidden">이전</span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
+                </svg>
+              </a>
+            </li>
+            <?php
+            for($i = $block_start; $i <= $block_end; $i++) {
+              if($i == $pageNumber) {
+                echo "<li class=\"page-item active\"><a href=\"payment.php?pageNumber=$i\" class=\"page-link\">$i</a></li>";
+              } else {
+                echo "<li class=\"page-item\"><a href=\"payment.php?pageNumber=$i\" class=\"page-link\">$i</a></li>";
+              }
+            }
+            ?>
+            <li class="page-item <?php echo $pageNumber == $total_page ? 'disabled' : ''; ?>">
+              <a class="page-link" href="<?php echo $pageNumber == $total_page ? '#' : 'payment.php?pageNumber=' . ($pageNumber + 1); ?>">
+                <span class="visually-hidden">다음</span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>
+                </svg>
+              </a>
+            </li>
+            <li class="page-item <?php echo $pageNumber == $total_page ? 'disabled' : ''; ?>">
+              <a class="page-link" href="<?php echo $pageNumber == $total_page ? '#' : 'payment.php?pageNumber=' . $total_page; ?>">
+                <span class="visually-hidden">마지막</span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-right" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M3.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L9.293 8 3.646 2.354a.5.5 0 0 1 0-.708"/>
+                  <path fill-rule="evenodd" d="M7.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8 7.646 2.354a.5.5 0 0 1 0-.708"/>
+                </svg>
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <!-- pagination(e) -->
       </div>
     </div>
   </main>
