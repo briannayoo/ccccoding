bd8092e45910a4adbb928434a0f69bf57ba02389
psh <sosohy38@gmail.com>
2024-05-21 13:08:22 +0900

    [mod]박소현/경로수정

--- coupon/coupon_down_ok.php
+++ coupon/coupon_down_ok.php
@@ -12,6 +12,6 @@ if(isset($_SESSION['UID'])){
   issue_coupon($mysqli, $userid, $cid, $name);
   echo "<script> alert('쿠폰이 발행되었습니다.'); location.href='/ccccoding/mypage/coupon.php'; </script>";
 }else {
-  echo "<script> alert('로그인 후 발급받으실 수 있습니다.'); location.href='/ccccoding/login.php'; </script>";
+  echo "<script> alert('로그인 후 발급받으실 수 있습니다.'); location.href='/ccccoding/member/login.php'; </script>";
 }
 ?>
